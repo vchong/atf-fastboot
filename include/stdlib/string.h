2ade89e7b0a50e941d8d4d4f10839af1d844dee7
Victor Chong <victor.chong@linaro.org>
2020-06-04 19:04:54 +0100

    stdlib: string: fix Clang build errors
    Fix below error:
    error: attribute declaration must precede definition [-Werror,-Wignored-attributes]
    
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- include/stdlib/string.h
+++ include/stdlib/string.h
@@ -49,16 +49,16 @@ typedef	__size_t	size_t;
 
 __BEGIN_DECLS
 
-void	*memchr(const void *, int, size_t) __pure;
-int	 memcmp(const void *, const void *, size_t) __pure;
+void	*memchr(const void *, int, size_t);
+int	 memcmp(const void *, const void *, size_t);
 void	*memcpy(void * __restrict, const void * __restrict, size_t);
 void	*memmove(void *, const void *, size_t);
 void	*memset(void *, int, size_t);
 
-char	*strchr(const char *, int) __pure;
-int	 strcmp(const char *, const char *) __pure;
-size_t	 strlen(const char *) __pure;
-int	 strncmp(const char *, const char *, size_t) __pure;
+char	*strchr(const char *, int);
+int	 strcmp(const char *, const char *);
+size_t	 strlen(const char *);
+int	 strncmp(const char *, const char *, size_t);
 int	 strcasecmp(const char *, const char *);
 
 __END_DECLS
